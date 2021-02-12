3241d27536e44b5005bd8c289fad1a16c9728313
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-02-12 14:44:18 -0600

    add board

--- src/components/TicTacToe.tsx
+++ src/components/TicTacToe.tsx
@@ -1,6 +1,40 @@
+import { Box, Grid } from '@chakra-ui/react';
 import React, { useState } from 'react';
 
 export const TicTacToe = () => {
   const [board, setBoard] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0]);
-  return <h1>TicTacToe</h1>;
+  return (
+    <Grid
+      w='500px'
+      mx='auto'
+      mt={10}
+      templateColumns='repeat(3,1fr)'
+      templateRows='repeat(3,1fr)'
+    >
+      {board.map((value) => {
+        return (
+          <Box
+            _hover={{
+              background: 'white',
+              cursor: 'pointer',
+              color: 'gray.800',
+              transition: 'all .2s ease',
+            }}
+            display='flex'
+            justifyContent='center'
+            alignItems='center'
+            fontSize={20}
+            p={4}
+            key={Math.random()}
+            height='100%'
+            width='100%'
+            bg='gray.600'
+            color='white'
+          >
+            {value}
+          </Box>
+        );
+      })}
+    </Grid>
+  );
 };
