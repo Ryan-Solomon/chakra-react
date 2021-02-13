9488ec0f812f5fc947f0f4c655dc7ff940bc4875
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-02-13 14:29:13 -0600

    test that board works

--- src/pages/ticTacToe/TicTacToe.test.tsx
+++ src/pages/ticTacToe/TicTacToe.test.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { TicTacToePage } from './TicTacToePage';
+import userEvent from '@testing-library/user-event';
 
 describe('TicTacToe', () => {
   test('renders', () => {
     render(<TicTacToePage />);
     expect(screen.getByText(/player/i)).toBeInTheDocument();
+  });
+  test('works', () => {
+    render(<TicTacToePage />);
+    const button = screen.getAllByRole('button')[0];
+    userEvent.click(button);
+    expect(screen.getByText('X')).toBeInTheDocument();
+    expect(screen.getByText('0')).toBeInTheDocument();
+    screen.debug();
   });
 });
