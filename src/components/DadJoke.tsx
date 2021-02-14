d29668c14347b95158299cb5fd701959d47c6b74
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-02-14 11:21:14 -0600

    spinner on error and show joke when no error

--- src/components/DadJoke.tsx
+++ src/components/DadJoke.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Text } from '@chakra-ui/react';
+import { Box, Text } from '@chakra-ui/react';
 import useSWR from 'swr';
+import { Spinner } from '@chakra-ui/react';
 
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
 const url = 'https://icanhazdadjoke.com/';
@@ -12,6 +13,17 @@ type TResponse = {
 export const DadJoke = () => {
   const { data, error } = useSWR<TResponse>(url, fetcher);
   if (error) return <div>failed to load</div>;
-  if (!data) return <div>loading...</div>;
-  return <Text>Dad Joke</Text>;
+  if (!data)
+    return (
+      <Box>
+        <Spinner size='lg' />
+      </Box>
+    );
+
+  const { joke } = data;
+  return (
+    <Box>
+      <Text>{joke}</Text>
+    </Box>
+  );
 };
