0332ef29423522a1661754ab4ad974c1a85ad27a
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-02-14 13:14:49 -0600

    test joke arriving

--- src/pages/dadjoke/DadJoke.test.tsx
+++ src/pages/dadjoke/DadJoke.test.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import { DadJokePage } from './DadJokePage';
 
 describe('<DadJokePage/>', () => {
-  test('Renders', () => {
+  test('Renders', async () => {
     render(<DadJokePage />);
-    screen.debug();
+    expect(screen.getByText(/loading/i)).toBeInTheDocument();
+    await waitFor(() => {
+      expect(screen.queryByText(/loading/i)).not.toBeInTheDocument();
+    });
+    expect(screen.getByText(/fetch joke/i)).toBeInTheDocument();
   });
 });
