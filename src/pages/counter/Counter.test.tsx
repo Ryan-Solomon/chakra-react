d1fd777f6e5b968f682f849b9591dc9222f74ae8
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-02-14 16:50:19 -0600

    test that counter works

--- src/pages/counter/Counter.test.tsx
+++ src/pages/counter/Counter.test.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import { CounterPage } from './CounterPage';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 describe('<CounterPage />', () => {
   test('Counter tests', () => {
     render(<CounterPage />);
-    screen.debug();
+    const increaseButton = screen.getByRole('button', { name: /increase/i });
+    const decreaseButton = screen.getByRole('button', { name: /decrease/i });
+    const clearButton = screen.getByRole('button', { name: /clear/i });
+    userEvent.click(increaseButton);
+    expect(screen.getByText(/count: 1/i)).toBeInTheDocument();
+    userEvent.click(increaseButton);
+    userEvent.click(decreaseButton);
+    expect(screen.getByText(/count: 1/i)).toBeInTheDocument();
+    userEvent.click(clearButton);
+    expect(screen.getByText(/count: 0/i)).toBeInTheDocument();
   });
 });
