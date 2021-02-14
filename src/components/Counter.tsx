3d53828bb457f7cc7de0fe5783725ed9be7afb89
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-02-14 14:55:40 -0600

    setup count reducer

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,6 +1,36 @@
 import { Text } from '@chakra-ui/react';
 import React from 'react';
 
+type TCountState = {
+  count: number;
+};
+
+type TAction =
+  | {
+      type: 'INCREASE';
+      payload: number;
+    }
+  | {
+      type: 'DECREASE';
+      payload: number;
+    }
+  | {
+      type: 'CLEAR';
+    };
+
+function countReducer(state: TCountState, action: TAction) {
+  switch (action.type) {
+    case 'INCREASE':
+      return { count: state.count + action.payload };
+    case 'DECREASE':
+      return { count: state.count - action.payload };
+    case 'CLEAR':
+      return { count: 0 };
+    default:
+      throw new Error('Action in countReducer is not supported');
+  }
+}
+
 export const Counter = () => {
   return <Text>Count</Text>;
 };
