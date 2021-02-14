777be37dbbaa1aa36d5a63652612fe4a8aae7fce
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-02-14 14:27:32 -0600

    test accordion

--- src/pages/accordion/Accordion.test.tsx
+++ src/pages/accordion/Accordion.test.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import { AccordionPage } from './AccordionPage';
 import { executionAsyncId } from 'async_hooks';
 
 describe('AccordionPage', () => {
-  test('renders', () => {
+  test('renders', async () => {
     render(<AccordionPage />);
     expect(screen.getByText(/loading/i)).toBeInTheDocument();
-
-    screen.debug();
+    await waitFor(() => {
+      expect(screen.queryByText(/loading/i)).not.toBeInTheDocument();
+    });
+    expect(screen.getAllByText(/margarita/i)[0]).toBeInTheDocument();
   });
 });
