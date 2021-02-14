935c423eaa9ef8c301495e38dc29e7c5f16a91a2
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-02-14 11:06:02 -0600

    setup swr

--- src/components/DadJoke.tsx
+++ src/components/DadJoke.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { Text } from '@chakra-ui/react';
+import useSWR from 'swr';
+
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 export const DadJoke = () => {
+  const { data, error } = useSWR('/api/user/123', fetcher);
   return <Text>Dad Joke</Text>;
 };
