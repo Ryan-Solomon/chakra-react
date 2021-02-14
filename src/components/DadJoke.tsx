9023f14d191d27b251ec3dcbaed1a506c4e11865
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-02-14 11:10:46 -0600

    statically type response

--- src/components/DadJoke.tsx
+++ src/components/DadJoke.tsx
@@ -3,8 +3,15 @@ import { Text } from '@chakra-ui/react';
 import useSWR from 'swr';
 
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
+const url = 'https://icanhazdadjoke.com/';
+type TResponse = {
+  id: string;
+  joke: string;
+};
 
 export const DadJoke = () => {
-  const { data, error } = useSWR('/api/user/123', fetcher);
+  const { data, error } = useSWR<TResponse>(url, fetcher);
+  if (error) return <div>failed to load</div>;
+  if (!data) return <div>loading...</div>;
   return <Text>Dad Joke</Text>;
 };
