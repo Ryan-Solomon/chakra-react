9f27f7b5427d844d0f0191d29b9af2a912f6e39e
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-02-14 11:03:59 -0600

    add dadjoke bp

---
+++ src/components/DadJoke.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Text } from '@chakra-ui/react';
+
+export const DadJoke = () => {
+  return <Text>Dad Joke</Text>;
+};
---
+++ src/pages/dadjoke/DadJokePage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { DadJoke } from '../../components/DadJoke';
+
+export const DadJokePage = () => {
+  return (
+    <>
+      <DadJoke />
+    </>
+  );
+};
